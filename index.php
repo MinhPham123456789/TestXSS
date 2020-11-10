b6d81cb86f8df38c284da65ca2dadab6abe8c114
MinhPham123456789 <46357749+MinhPham123456789@users.noreply.github.com>
2020-11-10 16:01:02 +0700

    Update index.php
    Show directory php

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
- <?php
-$txt = "PHP";
-echo "I love $txt!";
-?> 
+<?php
+$dir = (isset($_GET['dir']) ? $_GET['dir'] : ".");
+
+if (is_dir($dir)) {
+  echo "<h2>Index of $dir:</h2>\n";
+  $files = array_diff(scandir($dir), array('..', '.'));
+  usort($files, function($a, $b) {
+    if(is_dir($a) == is_dir($b))
+      return strnatcasecmp($a, $b);
+    else
+      return is_dir($a) ? -1 : 1;
+  });
+  echo "<a href='?dir=" . urlencode(dirname($dir)) . "'>..</a><br>\n";
+  foreach($files AS $file) {
+    echo "<a href='?dir=" . urlencode("{$dir}/{$file}") . "'>{$file}</a><br>\n";                                                                                                                                                                                                                                                                       
+  }                                                                                                                                                                                                                                                                                                                                                    
+} else if (is_file($dir)) {                                                                                                                                                                                                                                                                                                                            
+  header('Content-Description: File Transfer');                                                                                                                                                                                                                                                                                                        
+  header('Content-Type: text/javascript');                                                                                                                                                                                                                                                                                                    
+  header('Content-Disposition: attachment; filename="' . basename($dir) . '"');                                                                                                                                                                                                                                                                        
+  header('Expires: 0');                                                                                                                                                                                                                                                                                                                                
+  header('Cache-Control: must-revalidate');                                                                                                                                                                                                                                                                                                            
+  header('Pragma: public');                                                                                                                                                                                                                                                                                                                            
+  header('Content-Length: ' . filesize($dir));                                                                                                                                                                                                                                                                                                         
+  readfile($dir);
+}
+exit;
+?>
